98caf63ab2626b6cce7f9cc30f0ceafa80e2a950
brunoreyes2204 <brunoreyes2204@gmail.com>
2026-09-18 18:30:21 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include itgam image

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
+//funcion para manejar errores
 var createError = require('http-errors');
+//Importar el framework express
 var express = require('express');
+//Importa modulos para manejar rutas
 var path = require('path');
+//Importa modulos para manejar cookies
 var cookieParser = require('cookie-parser');
+//Importa modulos para manejar logs
 var logger = require('morgan');
 
+//Importar las rutas de la aplicación
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Crear la aplicación express
 var app = express();
 
-// view engine setup
+// Configurar el motor de vistas 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Configurar middlewares de la aplicación
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Configurar la carpeta de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Registramos las rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Capturamos los errores 404 y los enviamos al manejador de errores
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
